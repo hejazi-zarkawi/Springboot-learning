73801af9b11d73ca47fdea3ddc203beeca3974eb
hejazi-zarkawi <hejazimohammad764@gmail.com>
2026-06-11 15:13:51 +0530

    Implemented Dynamic query methods (Module 3)

---
+++ src/main/java/com/codingshuttle/hejazi/demo/module3_Hibernate_and_SpringJPA/repositories/ProductRepository.java
@@ -0,0 +1,20 @@
+package com.codingshuttle.hejazi.demo.module3_Hibernate_and_SpringJPA.repositories;
+
+import com.codingshuttle.hejazi.demo.module3_Hibernate_and_SpringJPA.entities.ProductEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductEntity,Long> {
+    List<ProductEntity> findByTitle(String title);
+
+    @Query("select e from ProductEntity e where e.title=:title and e.price=:price")
+    Optional<ProductEntity> findByTitleAndPrice(String title, BigDecimal price);
+
+    List<ProductEntity> findByTitleLike(String s);
+}
--- src/test/java/com/codingshuttle/hejazi/demo/DemoApplicationTests.java
+++ src/test/java/com/codingshuttle/hejazi/demo/DemoApplicationTests.java
@@ -1,13 +1,44 @@
 package com.codingshuttle.hejazi.demo;
 
+import com.codingshuttle.hejazi.demo.module3_Hibernate_and_SpringJPA.entities.ProductEntity;
+import com.codingshuttle.hejazi.demo.module3_Hibernate_and_SpringJPA.repositories.ProductRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+import java.sql.SQLOutput;
+import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	@Autowired
+	ProductRepository productRepository;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testRepository(){
+		ProductEntity productEntity= ProductEntity.builder().price(BigDecimal.valueOf(238L)).quantity(26)
+				.sku("cad874682yu").title("Cadbury Co").build();
+		ProductEntity saved= productRepository.save(productEntity);
+		System.out.println(saved);
+	}
+
+	@Test
+	void getRepository(){
+//		List<ProductEntity> entities= productRepository.findByTitle("parle");
+//		System.out.println(entities);
+
+//		Optional<ProductEntity> entity= productRepository.findByTitleAndPrice("Cadbury",BigDecimal.valueOf(30L));
+//		entity.ifPresent(product -> System.out.println(product));
+
+		List<ProductEntity> entities= productRepository.findByTitleLike("%Cad%");
+		entities.stream().forEach(entity-> System.out.println(entity));
 	}
 
 }
